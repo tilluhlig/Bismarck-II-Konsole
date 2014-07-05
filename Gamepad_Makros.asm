c42eaa44fb41b56562f723ab0e05282ccbbed65c
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-07-05 12:05:44 +0200

    open, unten und gedrueckt Befehle beschleunigt

--- Gamepad_Makros.asm
+++ Gamepad_Makros.asm
@@ -38,18 +38,12 @@
 ;######### prueft, ob eine Taste #############
 ;################ unten ist ##################
 ;#############################################
-;######## Bedarf: 30 Byte, 20 Takte ##########
+;######## Bedarf: 24 Byte, 15 Takte ##########
 ;#############################################
 .macro pruefe_taste_unten
 // Aufruf: outPort=@0, outPin=@1, outReg=@2, inPort=@3, inPort2=@4, inPin=@5, inReg=@6, ID=@7, THEN=@8, ELSE=@9
 // oder
 // Aufruf: TASTE, THEN=@8, ELSE=@9
-
-    ldi zh, high(TASTEN_ZUSTAENDE_TEMP)
-    ldi zh, low(TASTEN_ZUSTAENDE_TEMP)
-    ldi temp, @7
-    add zl, temp
-    adc zh, NULL
 
     // in als Eingang einstellen
     cbi @6,@5
@@ -64,12 +58,12 @@
     sbic @3, @5
     rjmp ist_nicht_unten
     ist_unten:
-    st Z, EINS
+    sts TASTEN_ZUSTAENDE+@7, EINS
     NOP
     rjmp @8
     
     ist_nicht_unten:
-    st Z, NULL
+    sts TASTEN_ZUSTAENDE+@7, NULL
     rjmp @9
 
 .endm
@@ -79,18 +73,12 @@
 ;######### prueft, ob eine Taste #############
 ;################ oben ist ###################
 ;#############################################
-;######## Bedarf: 30 Byte, 20 Takte ##########
+;######## Bedarf: 24 Byte, 15 Takte ##########
 ;#############################################
 .macro pruefe_taste_oben
 // Aufruf: outPort=@0, outPin=@1, outReg=@2, inPort=@3, inPort2=@4, inPin=@5, inReg=@6, ID=@7, THEN=@8, ELSE=@9
 // oder
 // Aufruf: TASTE, THEN=@8, ELSE=@9
-
-    ldi zh, high(TASTEN_ZUSTAENDE_TEMP)
-    ldi zh, low(TASTEN_ZUSTAENDE_TEMP)
-    ldi temp, @7
-    add zl, temp
-    adc zh, NULL
 
     // in als Eingang einstellen
     cbi @6,@5
@@ -105,10 +93,56 @@
     sbic @3, @5
     rjmp ist_nicht_unten
     ist_unten:
-    st Z, EINS
+    sts TASTEN_ZUSTAENDE+@7, EINS
     rjmp @9
     
     ist_nicht_unten:
-    st Z, NULL
+    sts TASTEN_ZUSTAENDE+@7, NULL
     rjmp @8
+.endm
+
+
+;#############################################
+;######### prueft, ob eine Taste #############
+;############ gedrueckt wurde ################
+;#############################################
+;######## Bedarf: 34 Byte, 21 Takte ##########
+;#############################################
+.macro pruefe_taste_gedrueckt
+// Aufruf: outPort=@0, outPin=@1, outReg=@2, inPort=@3, inPort2=@4, inPin=@5, inReg=@6, ID=@7, THEN=@8, ELSE=@9
+// oder
+// Aufruf: TASTE, THEN=@8, ELSE=@9
+
+    // in als Eingang einstellen
+    cbi @6,@5
+    // Pull-Up Widerstand einschalten
+    sbi @4,@5
+
+    // out als Ausgang einstellen
+    sbi @2, @1
+    // Ausgang auf 0
+    cbi @0, @1  
+
+    sbic @3, @5
+    rjmp ist_nicht_unten
+    ist_unten:
+    sts TASTEN_ZUSTAENDE+@7, EINS
+    lds temp, TASTEN_ZUSTAENDE+@7
+    cpi temp, 0
+    brne ist_nicht_unten2
+
+    NOP
+    NOP
+    NOP
+    rjmp @8
+    
+    ist_nicht_unten:
+    NOP
+    NOP
+    NOP
+
+    ist_nicht_unten2:
+    sts TASTEN_ZUSTAENDE+@7, NULL
+    rjmp @9
+
 .endm
