542aa4a043d28108c19314f6f922591652fe4e1f
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-07-05 21:05:01 +0200

    Befehle wieder eingeschalten

--- Bismarck-Konsole.asm
+++ Bismarck-Konsole.asm
@@ -111,7 +111,7 @@ super_taste_nicht_gedrueckt:*/
 
 
 // Scheinwerfer einstellen
-/*L_ZWEI_TASTE pruefe_taste_unten, l_zwei_taste_unten, l_zwei_taste_nicht_unten
+L_ZWEI_TASTE pruefe_taste_unten, l_zwei_taste_unten, l_zwei_taste_nicht_unten
 l_zwei_taste_unten:
 SUPER_TASTE pruefe_taste_gedrueckt, super_taste_gedrueckt, l_zwei_taste_nicht_unten
 super_taste_gedrueckt:
@@ -125,60 +125,50 @@ scheinwerfer_ausschalten:
 // alle Scheinwerfer ausschalten
 sts SCHEINWERFER, NULL
 LICHT_BEFEHL befehl_senden,1,0
-wait_ms 10
+wait_ms 100
 LICHT_BEFEHL befehl_senden,2,0
-wait_ms 10
+wait_ms 100
 LICHT_BEFEHL befehl_senden,3,0
-wait_ms 10
+wait_ms 100
 LICHT_BEFEHL befehl_senden,4,0
-wait_ms 10
+wait_ms 100
 LICHT_BEFEHL befehl_senden,5,0
-wait_ms 10
+wait_ms 100
 
 rjmp scheinwerfer_schalten_ende
 schweinwerfer_einschalten:
 // alle Scheinwerfer einschalten
 sts SCHEINWERFER, EINS
 LICHT_BEFEHL befehl_senden, 1, 1
-wait_ms 10
+wait_ms 100
 LICHT_BEFEHL befehl_senden, 2, 1
-wait_ms 10
+wait_ms 100
 LICHT_BEFEHL befehl_senden, 3, 1
-wait_ms 10
+wait_ms 100
 LICHT_BEFEHL befehl_senden, 4, 1
-wait_ms 10
+wait_ms 100
 LICHT_BEFEHL befehl_senden, 5, 1
-wait_ms 10
+wait_ms 100
 scheinwerfer_schalten_ende:
 sts AKTIV2, ALL
 rjmp ende_tasten
-l_zwei_taste_nicht_unten:*/
+l_zwei_taste_nicht_unten:
 
 
 // SUPER_TASTE pruefen (L)
 SUPER_TASTE pruefe_taste_gedrueckt, super_taste_gedrueckt2, super_taste_nicht_gedrueckt2
 super_taste_gedrueckt2:
 
+L_BEFEHL befehl_senden, 1, 255, 255, 255
+wait_ms 150
+L_BEFEHL befehl_senden, 2, 255, 255, 255
+wait_ms 150
+L_BEFEHL befehl_senden, 3, 255, 255, 255
+wait_ms 150
+L_BEFEHL befehl_senden, 4, 255, 255, 255
+wait_ms 150
 L_BEFEHL befehl_senden, 5, 255, 255, 255
 wait_ms 150
-LICHT_BEFEHL befehl_senden, 5, 1
-/*uart_send 'l'
-uart_send 'i'
-uart_send 'c'
-uart_send 'h'
-uart_send 't'
-uart_send ' '
-uart_send '5'
-uart_send ' '
-uart_send '1'
-uart_send ' '
-uart_send ' '
-uart_send ' '
-uart_send ' '
-uart_send ' '
-uart_send ' '*/
-
-
 
 sts AKTIV2, ALL
 rjmp ende_tasten
